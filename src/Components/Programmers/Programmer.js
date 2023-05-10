09047ad2269a5c1b58ae10430a061c998dcf8590
KhondokarAlMaruf <khondokaralmaruf01@gmail.com>
2023-05-11 02:42:12 +0600

    card design done

--- src/Components/Programmers/Programmer.js
+++ src/Components/Programmers/Programmer.js
@@ -3,12 +3,23 @@ import "./Programmer.css";
 
 const Programmer = (props) => {
   console.log(props);
-  const { id, img, name, description, date } = props.programmer;
+  const { id, img, name, description, date, icon } = props.programmer;
   return (
     <div className="progmmer">
       <img className="cover" src={img} alt="" />
-      <div className="icon">
-        <img className="icon-img" src={img} alt="" />
+      <div className="icon-container">
+        <div className="icon">
+          <img className="icon-img" src={icon} alt="" />
+          <div>
+            <p id="name">{name}</p>
+            <p>{date}</p>
+          </div>
+        </div>
+        <div>
+          <p>
+            05 min read <button>!</button>
+          </p>
+        </div>
       </div>
       <h2>{description}</h2>
       <a href="">Mark as read</a>
