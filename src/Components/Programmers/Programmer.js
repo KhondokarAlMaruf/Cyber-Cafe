3ac39caec35df24cf2bb62114bc15f7ed45f8d54
KhondokarAlMaruf <khondokaralmaruf01@gmail.com>
2023-05-11 02:07:18 +0600

    img design added

--- src/Components/Programmers/Programmer.js
+++ src/Components/Programmers/Programmer.js
@@ -5,9 +5,13 @@ const Programmer = (props) => {
   console.log(props);
   const { id, img, name, description, date } = props.programmer;
   return (
-    <div>
-      <img src={img} alt="" />
-      Name : {name}
+    <div className="progmmer">
+      <img className="cover" src={img} alt="" />
+      <div className="icon">
+        <img className="icon-img" src={img} alt="" />
+      </div>
+      <h2>{description}</h2>
+      <a href="">Mark as read</a>
     </div>
   );
 };
